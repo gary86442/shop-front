184f3ceea1ce32140122a7c6259949d9d9932824
Leechiayu <gary86442@gmail.com>
2023-07-26 15:22:04 +0800

    del: dist and rename e-shop-front in config

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/e-shop-front2/' : './'
+  base: process.env.NODE_ENV === 'production' ? '/e-shop-front/' : './'
 })
