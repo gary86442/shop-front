09d4bdf5cf7bed4b23301daa644dbccf733050be
Leechiayu <gary86442@gmail.com>
2023-07-26 15:13:25 +0800

    feat: edit config to shop2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Launched_pView from '../views/Launched_pView.vue'
 
